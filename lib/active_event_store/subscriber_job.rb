ad867db870cd1cca20ba610ac635b501a2a4805e
Aris Chamas <aris.chamakiotis@gmail.com>
2023-08-18 16:48:49 +0300

    Add support for Rails Event Store 2.11.1

--- lib/active_event_store/subscriber_job.rb
+++ lib/active_event_store/subscriber_job.rb
@@ -38,7 +38,7 @@ module ActiveEventStore
     end
 
     def perform(payload)
-      event = event_store.deserialize(**payload, serializer: ActiveEventStore.config.serializer)
+      event = event_store.deserialize(**payload&.transform_keys(&:to_sym), serializer: ActiveEventStore.config.serializer)
 
       event_store.with_metadata(**event.metadata.to_h) do
         subscriber.call(event)
